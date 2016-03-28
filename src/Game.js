e0e7b4ab69d3acbe1c135ae3c7e9acc4f8c06ccc
akirsanov <Alexander.Kirsanov@reltio.com>
2016-03-28 18:32:45 +0300

    slider has been introduced

--- src/Game.js
+++ src/Game.js
@@ -3,6 +3,7 @@ import Hand from './hand';
 import CardComponent from './components/Card';
 import Button from './components/Button';
 import React from 'react';
+import Slider from 'react-slick';
 class Game extends React.Component {
     constructor() {
         super();
@@ -90,7 +91,10 @@ class Game extends React.Component {
         };
         return (
             <div>
-                <div>{this.state.cardImages.map(x=> <CardComponent suite={x.suit} face={x.face} key={x.index}/>)}</div>
+                <Slider>
+                    <div>{this.state.cardImages.map(x=> <CardComponent suite={x.suit} face={x.face}
+                                                                       key={x.index}/>)}</div>
+                </Slider>
                 <div style={style}>{message}</div>
                 <div>
                     <Button disabled={this.state.game !== 'started'} action={() => this.action('higher')}
