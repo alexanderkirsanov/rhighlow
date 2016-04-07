c4916b256c0d3bee82a9d548ac8ef5b716400e56
akirsanov <Alexander.Kirsanov@reltio.com>
2016-04-07 10:45:33 +0500

    hand fixes

--- src/game.js
+++ src/game.js
@@ -2,11 +2,11 @@ const Game = (element)=> {
     const hand = Hand(5);
     const setup = () => {
         const deck = CardDeck();
-        for (let i = 0; i < this.hand.size; i++) {
-            this.hand.addCard(deck.deal());
+        for (let i = 0; i < hand.size; i++) {
+            hand.addCard(deck.deal());
         }
         this.nextCard = 1;
-        const {suit, face} = this.hand.getCard(0);
+        const {suit, face} = hand.getCard(0);
         const images = Array.from({length: 5}, (x, i)=> {
             return {index: i}
         });
