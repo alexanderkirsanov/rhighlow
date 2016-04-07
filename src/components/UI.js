ae0020fe4866bca0a87fad25dbe2e19e8a1381d6
akirsanov <Alexander.Kirsanov@reltio.com>
2016-04-07 11:56:16 +0500

    let's use hyperscript for component

--- src/components/UI.js
+++ src/components/UI.js
@@ -1,33 +1,13 @@
 import config from '../config';
+import h from 'hyperscript'
 
-const createElement = (tag, props)=> {
-    const element = document.createElement(tag);
-    for (let prop of Object.keys(props)) {
-        element[prop] = props[prop];
-    }
-    return element;
-};
-const Button = (props)=> {
-    const btn = createElement('button', {className: props.className});
-    const text = document.createTextNode(props.text || '');
-    btn.appendChild(text);
-    return btn;
-};
 
-const Card = (props) => {
-    const img = createElement('img', {
-        className: props.className,
-        src: config.makeUrl(props)
-    });
-    return img;
-};
+const Button = (props)=> h(`button.${props.className}`, {onclick: props.onClick});
+const Card = (props)=> h(`img.${props.className}`, {src: config.makeUrl(props)});
+
 const Spinner = ()=> {
-    const spinner = createElement('div', {
-        className: 'spinner'
-    });
-    [1, 2, 3, 4, 5].forEach(i=> {
-        spinner.appendChild(createElement('div', {className: `rect${i}`}));
-    });
-    return spinner;
+    const items = [1, 2, 3, 4, 5].map(i=> h(`div.rect${i}`));
+    return h('div.spinner',items);
 };
+
 export {Button, Card, Spinner};
--- src/main.js
+++ src/main.js
@@ -1,26 +1,18 @@
 import {Spinner} from './components/UI';
 import config from './config';
 import ImageLoader from './imageLoader';
+import h from 'hyperscript'
 
-const render = (parent, element) => {
-    while (parent.firstChild) {
-        parent.removeChild(parent.firstChild);
-    }
-    if (element) {
-        parent.appendChild(element);
-    }
-};
 const GameLoader = (element) => {
-    render(element, Spinner());
     const concat = Array.prototype.concat;
     const makeUrl = config.makeUrl;
     const images = config.suits.reduce((prev, suite) => concat.call(prev, config.faces.map(face => makeUrl({
         suite,
         face
     }))), [config.defaultImage]);
-
+    element.appendChild(Spinner());
     ImageLoader.loadImages(images).then(()=> {
-        render(element);
+        h('div#game', h('div'));
     });
 };
 GameLoader(document.getElementById('game'));
