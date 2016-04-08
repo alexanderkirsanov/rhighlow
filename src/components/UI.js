03f9a3e8c4086b93099c1656c655be606ab1cd2e
akirsanov <Alexander.Kirsanov@reltio.com>
2016-04-08 17:50:45 +0500

    card intial implementation

--- src/components/UI.js
+++ src/components/UI.js
@@ -3,11 +3,33 @@ import h from 'hyperscript'
 
 
 const Button = (props)=> h(`button.${props.className}`, {onclick: props.onClick});
-const Card = (props)=> h(`img.${props.className}`, {src: config.makeUrl(props)});
+const Card = (props)=>
+    h('div', {
+            style: {
+                position: 'absolute',
+                width: `${props.width}px`,
+                left: `${props.left}px`,
+                top: `${props.top}px`,
+                'border-radius': '3%',
+                overflow: 'hidden',
+                height: `${props.height}px`
+            }
+        },
+        h(`img.${props.className}`,
+            {
+                src: config.makeUrl(props)
+            }, {
+                style: {
+                    'max-width': '100%',
+                    'max-height': '100%'
+                }
+            }
+        )
+    );
 
 const Spinner = ()=> {
     const items = [1, 2, 3, 4, 5].map(i=> h(`div.rect${i}`));
-    return h('div.spinner',items);
+    return h('div.spinner', items);
 };
 
 export {Button, Card, Spinner};
--- src/components/deck.js
+++ src/components/deck.js
@@ -1,33 +1,72 @@
-import h from 'hyperscript';
-
+import {Card} from './UI';
 const deck = (element, board) => {
-    const ORIGINAL_WIDTH = 1100;
-    const ORIGINAL_HEIGHT = 700;
+    const getCoordinates = (originalRight, originalWidth, originalHeight) => {
+        const ORIGINAL_WIDTH = 1100;
+        const ORIGINAL_HEIGHT = 700;
+        const left = Math.ceil(board.width * (ORIGINAL_WIDTH - originalRight) / ORIGINAL_WIDTH);
+        const width = Math.ceil(board.width * originalWidth / ORIGINAL_WIDTH);
+        const height = Math.ceil(width * originalHeight / originalWidth);
+        const maxHeight = Math.ceil(width * ORIGINAL_HEIGHT / originalWidth);
+        const top = Math.ceil((maxHeight - height) / 2);
+        return {left, top, width, height};
+    };
+
     const firstCard = () => {
         const ORIGINAL_RIGHT = 1041;
         const ORIGINAL_FIRST_WIDTH = 260;
         const ORIGINAL_FIRST_HEIGHT = 359;
-        console.log(board.width);
-        const left = Math.ceil(board.width * (ORIGINAL_WIDTH - ORIGINAL_RIGHT) / ORIGINAL_WIDTH);
-        const width = Math.ceil(board.width * ORIGINAL_FIRST_WIDTH / ORIGINAL_WIDTH);
-        const height = Math.ceil(width * ORIGINAL_FIRST_HEIGHT / ORIGINAL_FIRST_WIDTH);
-        return h(`div.firstCard`, {
-            style: {
-                'background-color': '#2f2',
-                position: 'absolute',
-                width: `${width}px`,
-                left: `${left}px`,
-                height: `${height}px`
-            }
-        });
+        const coordinates = getCoordinates(ORIGINAL_RIGHT, ORIGINAL_FIRST_WIDTH, ORIGINAL_FIRST_HEIGHT);
+        return Card(Object.assign(coordinates, {className: 'firstCard'}));
     };
+
+    const secondCard = () => {
+        const ORIGINAL_RIGHT = 1019;
+        const ORIGINAL_FIRST_WIDTH = 286;
+        const ORIGINAL_FIRST_HEIGHT = 394;
+        const coordinates = getCoordinates(ORIGINAL_RIGHT, ORIGINAL_FIRST_WIDTH, ORIGINAL_FIRST_HEIGHT);
+        return Card(Object.assign(coordinates, {className: 'secondCard'}));
+    };
+
+    const thirdCard = () => {
+        const ORIGINAL_RIGHT = 957;
+        const ORIGINAL_FIRST_WIDTH = 325;
+        const ORIGINAL_FIRST_HEIGHT = 450;
+        const coordinates = getCoordinates(ORIGINAL_RIGHT, ORIGINAL_FIRST_WIDTH, ORIGINAL_FIRST_HEIGHT);
+        return Card(Object.assign(coordinates, {className: 'thirdCard'}));
+    };
+
+    const centralCard = () => {
+        const ORIGINAL_RIGHT = 847;
+        const ORIGINAL_FIRST_WIDTH = 406;
+        const ORIGINAL_FIRST_HEIGHT = 560;
+        const coordinates = getCoordinates(ORIGINAL_RIGHT, ORIGINAL_FIRST_WIDTH, ORIGINAL_FIRST_HEIGHT);
+        return Card(Object.assign(coordinates, {className: 'centralCard'}));
+    };
+
+
+    const bigCard = () => {
+        const ORIGINAL_RIGHT = 483;
+        const ORIGINAL_FIRST_WIDTH = 460;
+        const ORIGINAL_FIRST_HEIGHT = 633;
+        const coordinates = getCoordinates(ORIGINAL_RIGHT, ORIGINAL_FIRST_WIDTH, ORIGINAL_FIRST_HEIGHT);
+        return Card(Object.assign(coordinates, {className: 'bigCard'}));
+    };
+
+
     const render = (state) => {
+
+
         while (element.firstChild) {
             element.removeChild(element.firstChild);
         }
-        element.appendChild(firstCard());
+        const cards = [firstCard(),
+            secondCard(),
+            thirdCard(),
+            centralCard(),
+            bigCard()];
+        cards.forEach(card => element.appendChild(card));
 
-    }
+    };
     return {render};
 }
 
