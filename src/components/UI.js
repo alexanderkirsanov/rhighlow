5cbf311ca7c4971a15ff021a081a9ec8a9f4bab5
Alexander.Kirsanov <Alexander.Kirsanov@reltio.com>
2016-04-10 20:34:36 +0300

    Status has been introduced

--- src/components/UI.js
+++ src/components/UI.js
@@ -21,12 +21,28 @@ const cardStyle = (props)=> {
 };
 const Card = (props)=> h('div', cardStyle(props));
 
-const ActionCard = (props) => h('div', [h('div.high'),h('div.low')], cardStyle(props));
+const ActionCard = (props) => h('div', [h('div.high'), h('div.low')], cardStyle(props));
 
-
+const Status = (props) => {
+    const opened = props.cards.filter(x=>x.open);
+    return h(
+        'div.status',
+        [
+            h('div.firstLine',
+                [
+                    h('p', 'Cards'),
+                    h('p', `${opened}/${props.cards.length}`)
+                ]
+            ),
+            h('progress.cardProgress', {max:props.cards.length, value: opened})
+        ]);
+};
+const Info = (props) => {
+    return h('div.info', [h('div.logo'), Status(props)]);
+};
 const Spinner = ()=> {
     const items = [1, 2, 3, 4, 5].map(i=> h(`div.rect${i}`));
     return h('div.spinner', items);
 };
 
-export {Button, ActionCard, Card, Spinner};
+export {Button, ActionCard, Card, Spinner, Info};
--- src/components/deck.js
+++ src/components/deck.js
@@ -1,10 +1,10 @@
 import {Card, ActionCard} from './UI';
 import move from 'move-js';
 const deck = (element, board) => {
-    const getCoordinates = (originalRight, originalWidth, originalHeight) => {
+    const getCoordinates = (originalLeft, originalWidth, originalHeight) => {
         const ORIGINAL_WIDTH = 1100;
         const ORIGINAL_HEIGHT = 700;
-        const left = Math.ceil(board.width * (ORIGINAL_WIDTH - originalRight) / ORIGINAL_WIDTH);
+        const left = Math.ceil(board.width * originalLeft / ORIGINAL_WIDTH);
         const width = Math.ceil(board.width * originalWidth / ORIGINAL_WIDTH);
         const height = Math.ceil(width * originalHeight / originalWidth);
         const maxHeight = Math.ceil(width * ORIGINAL_HEIGHT / originalWidth);
@@ -13,45 +13,45 @@ const deck = (element, board) => {
     };
 
     const firstCard = () => {
-        const ORIGINAL_RIGHT = 1041;
         const ORIGINAL_FIRST_WIDTH = 260;
+        const ORIGINAL_LEFT = 1076 - ORIGINAL_FIRST_WIDTH;
         const ORIGINAL_FIRST_HEIGHT = 359;
-        const coordinates = getCoordinates(ORIGINAL_RIGHT, ORIGINAL_FIRST_WIDTH, ORIGINAL_FIRST_HEIGHT);
+        const coordinates = getCoordinates(ORIGINAL_LEFT, ORIGINAL_FIRST_WIDTH, ORIGINAL_FIRST_HEIGHT);
         return Card(Object.assign(coordinates, {className: 'firstCard'}));
     };
 
     const secondCard = () => {
-        const ORIGINAL_RIGHT = 1019;
         const ORIGINAL_FIRST_WIDTH = 286;
+        const ORIGINAL_LEFT = 1061 - ORIGINAL_FIRST_WIDTH;
         const ORIGINAL_FIRST_HEIGHT = 394;
-        const coordinates = getCoordinates(ORIGINAL_RIGHT, ORIGINAL_FIRST_WIDTH, ORIGINAL_FIRST_HEIGHT);
+        const coordinates = getCoordinates(ORIGINAL_LEFT, ORIGINAL_FIRST_WIDTH, ORIGINAL_FIRST_HEIGHT);
         return Card(Object.assign(coordinates, {className: 'secondCard'}));
     };
 
     const thirdCard = () => {
-        const ORIGINAL_RIGHT = 957;
         const ORIGINAL_FIRST_WIDTH = 325;
+        const ORIGINAL_LEFT = 1025- ORIGINAL_FIRST_WIDTH;
         const ORIGINAL_FIRST_HEIGHT = 450;
-        const coordinates = getCoordinates(ORIGINAL_RIGHT, ORIGINAL_FIRST_WIDTH, ORIGINAL_FIRST_HEIGHT);
+        const coordinates = getCoordinates(ORIGINAL_LEFT, ORIGINAL_FIRST_WIDTH, ORIGINAL_FIRST_HEIGHT);
         return Card(Object.assign(coordinates, {className: 'thirdCard'}));
     };
 
     const centralCard = (state = {suite:null, face:null}) => {
         const {suite, face} = state;
-        const ORIGINAL_RIGHT = 847;
         const ORIGINAL_FIRST_WIDTH = 406;
+        const ORIGINAL_LEFT = 978-ORIGINAL_FIRST_WIDTH;
         const ORIGINAL_FIRST_HEIGHT = 560;
-        const coordinates = getCoordinates(ORIGINAL_RIGHT, ORIGINAL_FIRST_WIDTH, ORIGINAL_FIRST_HEIGHT);
+        const coordinates = getCoordinates(ORIGINAL_LEFT, ORIGINAL_FIRST_WIDTH, ORIGINAL_FIRST_HEIGHT);
         return ActionCard(Object.assign(coordinates, {className: 'centralCard'}));
     };
 
 
     const bigCard = (state = {suite:null, face:null}) => {
         const {suite, face} = state;
-        const ORIGINAL_RIGHT = 483;
         const ORIGINAL_FIRST_WIDTH = 460;
+        const ORIGINAL_LEFT = 640 - ORIGINAL_FIRST_WIDTH;
         const ORIGINAL_FIRST_HEIGHT = 633;
-        const coordinates = getCoordinates(ORIGINAL_RIGHT, ORIGINAL_FIRST_WIDTH, ORIGINAL_FIRST_HEIGHT);
+        const coordinates = getCoordinates(ORIGINAL_LEFT, ORIGINAL_FIRST_WIDTH, ORIGINAL_FIRST_HEIGHT);
         return Card(Object.assign(coordinates, {className: 'bigCard', suite, face}));
     };
 
